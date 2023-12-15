ca1b8f48f57b88e2643294628d4258b829858004
Микола Пащенко <maxbaxpax@gmail.com>
2023-12-15 19:17:21 +0200

    add file

---
+++ hw 5.py
@@ -0,0 +1,11 @@
+class Shifr:
+    def init(self, num1, num2, num3):
+        self.num1 = num1
+        self.num2 = num2
+        self.num3 = num3
+
+o = Shifr()
+l = []
+print(dir(l))
+print(dir(o))
+print(hasattr(o, 'num2'))
