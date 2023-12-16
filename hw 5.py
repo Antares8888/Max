072a6f530d29d8999733f0256c63961985ff1069
Микола Пащенко <maxbaxpax@gmail.com>
2023-12-16 12:48:43 +0200

    add file

--- hw 5.py
+++ hw 5.py
@@ -1,3 +1,6 @@
+import colorama
+
+
 class Shifr:
     def init(self, num1, num2, num3):
         self.num1 = num1
@@ -9,3 +12,5 @@ l = []
 print(dir(l))
 print(dir(o))
 print(hasattr(o, 'num2'))
+print(callable(colorama.colorama_text()))
+print(dir(colorama.AnsiToWin32))
